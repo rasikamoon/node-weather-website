14bde03abf26bd64f3b60aae6b183eedefadbaf9
Rasika Moon <rasikarmoon13@gmail.com>
2020-05-04 01:22:41 +0530

    Removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side js file is loaded')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#messageOne')
@@ -17,7 +15,7 @@ weatherForm.addEventListener('submit', (e) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
-                
+
             } else {
                 messageOne.textContent = data.location
                 messageTwo.textContent = data.forecast
